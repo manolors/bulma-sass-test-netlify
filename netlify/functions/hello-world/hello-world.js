98017a1d2b519b287605ae20e1283bce9f91a3f1
Manuel Rodríguez Santana <lolo@turitop.com>
2022-04-20 18:25:23 +0100

    change netlify.toml

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -7,7 +7,7 @@ const handler = async (event) => {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}!`, lala:`lala`, processenv: process.env }),
+      body: JSON.stringify({ message: `Hello ${subject}!`, processenv: process.env }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
