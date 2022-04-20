7c4d77492b732a5afa642cf5c33da0c069a03240
Manuel Rodríguez Santana <lolo@turitop.com>
2022-04-20 18:15:04 +0100

    add dotenv

--- scripts/createEnv.js
+++ scripts/createEnv.js
@@ -1,2 +1,6 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+import dotenv from 'dotenv';
+dotenv.config();
+
 const fs = require('fs')
 fs.writeFileSync('./.env', `SECRETO=${process.env.SECRETO}\n`)
