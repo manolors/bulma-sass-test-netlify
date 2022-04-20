19f92721b34decf91b6d2699111b932b34f1b2c1
Manuel Rodríguez Santana <lolo@turitop.com>
2022-04-20 18:12:33 +0100

    Add processenv

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,12 +1,13 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const env = require("dotenv").config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 const handler = async (event) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}!`, lala:`lala`, env: JSON.stringify(env), processenv: JSON.stringify(process.env) }),
+      body: JSON.stringify({ message: `Hello ${subject}!`, lala:`lala`, processenv: process.env }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
